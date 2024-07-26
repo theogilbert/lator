774e455fb76038a63aacacbf8fbd34f4fe4bfb2b
theogilbert <theovgilbert@gmail.com>
2024-07-26 08:49:35 +0200

    add AST module to represent expression

---
+++ src/engine/ast.rs
@@ -0,0 +1,61 @@
+use crate::engine::number::Number;
+
+pub enum Ast {
+    Number(Number),
+    Operator(OperatorType, Box<Ast>, Box<Ast>)
+}
+
+impl Ast {
+    pub fn resolve(&self) -> Number {
+        match self {
+            Ast::Number(num) => *num,
+            Ast::Operator(op, lhs, rhs) => {
+                op.calculate(lhs.resolve(), rhs.resolve())
+            }
+        }
+    }
+}
+
+pub enum OperatorType {
+    Add
+}
+
+impl OperatorType {
+    fn calculate(&self, lhs: Number, rhs: Number) -> Number {
+        match self {
+            OperatorType::Add => lhs + rhs
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case("0")]
+    #[case("123")]
+    #[case("123.456")]
+    fn should_parse_str_into_number(#[case] num_repr: &str) {
+        let num_node = number(num_repr);
+        assert_eq!(num_node.resolve().to_string(), num_repr);
+    }
+
+    #[test]
+    fn should_add_leading_zero_when_number_starts_with_dot() {
+        let num_node = number(".456");
+        assert_eq!(num_node.resolve().to_string(), "0.456");
+    }
+
+    #[test]
+    fn should_add_numbers() {
+        let add = Ast::Operator(OperatorType::Add, number(".456").into(), number("18.1").into());
+        assert_eq!(add.resolve().to_string(), "18.556");
+    }
+
+    fn number(repr: &str) -> Ast {
+        Ast::Number(Number::from_str(repr).unwrap())
+    }
+}
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,35 +1,37 @@
 use thiserror::Error;
 
-use crate::engine::token::{tokenize, TokenType};
+use crate::engine::ast::{Node, NumberNode};
+use crate::engine::token::tokenize;
 
 mod token;
+mod ast;
 mod number;
 
 
 #[derive(Error, Debug, PartialEq)]
 pub enum Error {
+    #[error("this type of expression is not yet supported")]
+    UnsupportedExpression(),
     #[error("number expression {0} has an invalid format: {1}")]
     InvalidNumberExpr(String, String),
 }
 
 /** Evaluates textual expressions */
-pub fn evaluate(expr: &str) -> Result<String, ()> {
+pub fn evaluate(expr: &str) -> Result<String, Error> {
     let tokens = tokenize(expr);
 
     if tokens.len() != 1 {
-        return Err(());
+        return Err(Error::UnsupportedExpression());
     }
 
-    let only_token = tokens.first().unwrap();
-    match only_token.token_type() {
-        TokenType::Number => Ok(only_token.content().to_string()),
-        _ => Err(())
-    }
+    Ok(NumberNode::from_str(tokens[0].content())?.resolve().to_string())
 }
 
 
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
+
     use super::evaluate;
 
     #[test]
@@ -37,8 +39,10 @@ mod tests {
         assert!(evaluate("invalid").is_err());
     }
 
-    #[test]
-    fn test_numerical_expr() {
-        assert_eq!(evaluate("42"), Ok("42".into()));
+
+    #[rstest]
+    #[case("123", "123")]
+    fn test_should_evaluate_valid_expressions(#[case] num_repr: &str, #[case] result: &str) {
+        assert_eq!(evaluate(num_repr), Ok(result.into()));
     }
 }
