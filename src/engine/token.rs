68f4ea4bdf3927fbd102640aee32d8fe962501f4
theogilbert <theovgilbert@gmail.com>
2024-07-27 15:52:03 +0200

    ignore whitespaces in expressions

--- src/engine/token.rs
+++ src/engine/token.rs
@@ -2,12 +2,13 @@ use std::collections::HashMap;
 
 use lazy_static::lazy_static;
 use regex::Regex;
+
 use crate::engine::operator::OperatorType;
 
 /// A TokenType indicates the semantic nature of a token.
 #[derive(Eq, PartialEq, Debug, Copy, Clone, Hash)]
 pub enum TokenType {
-    /// Indicates that a part of an expression is invalid.
+    /// Represents an invalid sequence of characters.
     Invalid,
     /// Classifies a token as either an integer or a decimal numeral.
     Number,
@@ -54,6 +55,10 @@ pub fn tokenize(expression: &str) -> Vec<Token> {
     let mut cursor = 0;
 
     while cursor < expression.len() {
+        cursor += measure_leading_whitespace(&expression[cursor..]);
+        if cursor >= expression.len() {
+            break;
+        }
         let next_token = produce_next_token(&expression[cursor..], cursor);
         cursor += next_token.length();
         tokens.push(next_token);
@@ -63,6 +68,7 @@ pub fn tokenize(expression: &str) -> Vec<Token> {
 }
 
 lazy_static! {
+    static ref WHITESPACE_PATTERN: Regex = Regex::new(r"^\s+").unwrap();
     /// TOKEN_PATTERNS represent the list of tokens parseable from the parser.
     static ref TOKEN_PATTERNS: HashMap<TokenType, Regex> = HashMap::from([
         (
@@ -74,6 +80,12 @@ lazy_static! {
             Regex::new(r"\+").unwrap()
         ),
     ]);
+}
+
+/// Returns the length of an eventual leading whitespace in the given expression.
+fn measure_leading_whitespace(expr: &str) -> usize {
+    let whitespace_prefix_match = WHITESPACE_PATTERN.find(expr);
+    whitespace_prefix_match.map(|m| m.len()).unwrap_or(0)
 }
 
 /// Produce a token starting from the start of the given expression.\
@@ -157,13 +169,8 @@ mod tests {
 
     #[test]
     fn should_evaluate_operator_as_valid_token() {
-        assert_eq!(
-            vec![str_to_token(
-                "+",
-                TokenType::Operator(OperatorType::Addition)
-            )],
-            tokenize("+")
-        );
+        let add_token = str_to_token("+", TokenType::Operator(OperatorType::Addition));
+        assert_eq!(vec![add_token], tokenize("+"));
     }
 
     #[rstest]
@@ -175,6 +182,17 @@ mod tests {
     ])]
     fn should_evaluate_expression_as_sequence(#[case] expr: &str, #[case] tokens: Vec<Token>) {
         assert_eq!(tokens, tokenize(expr));
+    }
+
+    #[test]
+    fn should_ignore_white_spaces() {
+        let expr = " 1   + \t\t\t   \x0a2 ";
+        let expected_sequence = vec![
+            Token::new(TokenType::Number, "1", 1),
+            Token::new(TokenType::Operator(OperatorType::Addition), "+", 5),
+            Token::new(TokenType::Number, "2", 14),
+        ];
+        assert_eq!(expected_sequence, tokenize(expr));
     }
 
     fn str_to_token(expr: &str, token_type: TokenType) -> Token {
